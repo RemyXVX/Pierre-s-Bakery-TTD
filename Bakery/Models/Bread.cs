d7e189367b76fe7b3e3ae9c953e871a8f7aefddb
RemyXVX <remyfranciscoflores@gmail.com>
2022-11-28 12:24:29 -0800

    Test 2 passed, created a quantity of bread

--- Bakery/Models/Bread.cs
+++ Bakery/Models/Bread.cs
@@ -2,6 +2,13 @@ namespace PierreBakes.Models
 {
   public class Bread
   {
+    public int CostOfBread {get; set;}
+    public int QuantityOfBread {get; set;}
 
+    public Bread(int qtyBread)
+    {
+      QuantityOfBread = qtyBread;
+    }
   }
+
 }
