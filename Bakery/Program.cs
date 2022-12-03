06237e093ed52b2b0319ba6ea28313c82330ad18
RemyXVX <remyfranciscoflores@gmail.com>
2022-12-03 00:32:21 -0800

    The start of a beautiful console UI

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -7,7 +7,45 @@ namespace PierreBakes
   {
     public static void Main()
     {
-      
+      Console.WriteLine("--------------------------------------------------");
+      Console.WriteLine("//////////////////////////////////////////////////");
+      Console.WriteLine("Welcome to land of carbs, this be Pierre's Bakery!");
+      Console.WriteLine("//////////////////////////////////////////////////");
+      Console.WriteLine("--------------------------------------------------");
+      Console.WriteLine("");
+      Console.WriteLine("");
+      Console.WriteLine("--------------------------------------------------");
+      Console.WriteLine("//////////////////////////////////////////////////");
+      Console.WriteLine(" ----- We have bake goods for sale!!");
+      Console.WriteLine(" ----- Our bread starting at $5");
+      Console.WriteLine(" ----- Or try our pastry for $2 a pop!");
+      Console.WriteLine("//////////////////////////////////////////////////");
+      Console.WriteLine("--------------------------------------------------");
+      Console.WriteLine("");
+      Console.WriteLine("");
+      Console.WriteLine("--------------------------------------------------");
+      Console.WriteLine("//////////////////////////////////////////////////");
+      Console.WriteLine("!!~~!! SPECIAL IF YOU BUY MUTLIPLE AT A TIME !!~~!!");
+      Console.WriteLine("//////////////////////////////////////////////////");
+      Console.WriteLine("--------------------------------------------------");
+      Console.WriteLine("So, are you intrested on getting some? \n(Enter 'Y' for yes, enter to exit)");
+      string order = Console.ReadLine();
+
+      if (order == "y")
+      {
+        Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        Console.WriteLine("What are we getting?");
+        Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        Console.WriteLine("");
+      }
+      else
+      {
+        Console.WriteLine("See ya later fooo!");
+      }
     }
   }
 }
